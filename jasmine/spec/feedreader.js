6945839cb6a609026d380bcb7057d4cc7d5dc5cd
Olena Kuziaieva <kusaeva@gmail.com>
2019-09-05 22:08:12 +0200

    Add test to check if the content is changed by loading new feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,26 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -45,7 +24,7 @@ $(function() {
         it('elemnt is hidden by default', function() {
             expect(document.body.classList.contains('menu-hidden')).toBe(true);
         });
-
+        
         it('changes visibility', function() {
             var menuIconElement = document.getElementsByClassName('menu-icon-link')[0];
             menuIconElement.click(); 
@@ -60,7 +39,7 @@ $(function() {
         
         beforeEach(function(done) {
             loadFeed(0, function() {
-                console.log('Work is finished!');
+                // Callback when the feed has been loaded
                 done();
             });
         });
@@ -71,10 +50,21 @@ $(function() {
         });
     }); 
     
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    describe('New Feed Selection', function() {
+        it('the content changes', function(done) {
+            var contentLoaded = function() {
+                 // save result of loading feeds
+                var loadFeedZeroResult = document.querySelector('.feed').innerHTML; 
+                
+                loadFeed(1, function () {
+                    var loadFeedFirstResult = document.querySelector('.feed').innerHTML;
+                    
+                    // compare saved result with new result
+                    expect(loadFeedZeroResult !== loadFeedFirstResult).toBe(true);
+                    done();
+                });
+            };
+            loadFeed(0, contentLoaded);
+        });
+    });
 }());
